6a07eaddc3a9850ed1ee0da49e5ff69b9a1d09d4
Mwangiboyle <mwangiboyle4@gmail.com>
2025-06-07 10:34:42 +0300

    added new comment on the file

--- main2.py
+++ main2.py
@@ -3,8 +3,10 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 
+#load environment variables
 load_dotenv()
 
+#load openai api keys 
 api_key = os.getenv("OPENAI_API_KEY")
 
 client = OpenAI(api_key=api_key)
